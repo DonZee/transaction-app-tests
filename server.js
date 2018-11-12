6274502c317b43daa6b2314e57cbc8b005b3ceeb
Troy Amelotte <troyamelotte@gmail.com>
2018-11-12 08:50:39 -0700

    asdf

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require("path");
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
+const cors = require('cors');
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors());
 app.set('view engine', 'ejs');
 
 var routes_setter = require('./config/routes.js');
